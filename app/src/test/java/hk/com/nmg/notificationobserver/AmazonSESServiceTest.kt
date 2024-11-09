c927a000e2800cad8bc75fee7753d4703e40274f
James Kong <fishkingsin@gmail.com>
2024-11-09 17:47:10 +0800

    WIP

--- app/src/test/java/hk/com/nmg/notificationobserver/AmazonSESServiceTest.kt
+++ app/src/test/java/hk/com/nmg/notificationobserver/AmazonSESServiceTest.kt
@@ -4,16 +4,29 @@ import android.util.Log
 import com.google.gson.Gson
 import dev.tools.screenlogger.ScreenLog
 import org.junit.Test
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class AmazonSESServiceTest {
 
     @Test
     fun run() {
+
+        val timestamp = System.currentTimeMillis()
+        val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        val instant = Instant.ofEpochMilli(timestamp)
+
+// Adding the timezone information to be able to format it (change accordingly)
+        val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+
         val list = listOf(
             AppPushModel(
-                appName = "App Name",
-                date = "Date",
-                receivedTime = "Received Time",
+                appName = "HK01",
+                date = dateFormatter.format(date),
+                receivedTime = timeFormatter.format(date),
                 appPushTitle = "App Push Title",
                 appPushContent = "App Push Content"
             )
